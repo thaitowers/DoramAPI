1d14c050e13ed2c95c6de80b361d42061861227f
thaitowers <134313833+thaitowers@users.noreply.github.com>
2023-10-13 17:59:57 -0300

    RestfulAPI para trackeamento de doramas assistidos.

---
+++ src/main/java/Thai/To/DoramaAPI/DoramaApiApplication.java
@@ -0,0 +1,13 @@
+package Thai.To.DoramaAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoramaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoramaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Thai/To/DoramaAPI/DoramaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Thai.To.DoramaAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoramaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
